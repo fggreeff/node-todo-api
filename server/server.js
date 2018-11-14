2397cbe60fbb186d4177480268d9422bd00eab92
Francois Greeff <francois.greeff@bglgroup.co.uk>
2018-11-14 14:23:09 +0000

    Add express & body parser

--- server/server.js
+++ server/server.js
@@ -1,3 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
 const { mongoose } = require('./db/mongoose')
 const { User } = require('./models/todo')
 const { Todo } = require('./models/user')
+
+var app = express()
+
+const port = 4000
+app.listen(port, () => {
+  console.log(`started on port ${port}`)
+})
