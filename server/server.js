5c11b0e5e6beab9bcfd2f92462c17caba8486f03
Francois <francois.greeff@bglgroup.co.uk>
2018-11-14 12:46:12 +0000

    Setup mongoose and schema (#1)
    * Create todo schema
    
    * Save todo doc to mongo
    
    * Refactor save logic
    
    * Add validation to todo model
    
    * Add user model with validation

---
+++ server/server.js
@@ -0,0 +1,61 @@
+var mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+mongoose.connect(
+  'mongodb://localhost:27017/TodoApp',
+  { useNewUrlParser: true }
+)
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    require: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+})
+
+var newTodo = new Todo({
+  text: 'edit this video'
+  //   completed: true,
+  //   completedAt: 123
+})
+
+newTodo.save().then(
+  doc => {
+    console.log(`save Todo ${JSON.stringify(doc, undefined, 2)}`)
+  },
+  e => {
+    console.log('unable to save Todo', e)
+  }
+)
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    require: true,
+    minlength: 5,
+    trim: true
+  }
+})
+
+var newUser = new User({
+  email: 'Jane@gmail.com'
+})
+
+newUser.save().then(
+  doc => {
+    console.log(`saved User ${JSON.stringify(doc, undefined, 2)}`)
+  },
+  err => {
+    console.log('unable to save User', e)
+  }
+)
