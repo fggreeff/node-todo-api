5272f9e9b9721cda2e62743373866f30ba68a80c
Francois <francois.greeff@bglgroup.co.uk>
2018-12-05 12:32:50 +0000

    Setup todo api (#3)
    * Add express & body parser
    
    * Create todo route
    
    * Save json post req for todo item
    
    * Add test packages
    
    * Add POST /route tests
    
    * Add GET /todos route
    
    * Add tests for GET /todos
    
    * Add GET /todos/:id
    
    * Add test cases for  GET /todos/:id
    
    * Setup package.json for  Heroku
    
    * Update readme

--- server/server.js
+++ server/server.js
@@ -1,3 +1,63 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { ObjectID } = require('mongodb')
+
 const { mongoose } = require('./db/mongoose')
-const { User } = require('./models/todo')
-const { Todo } = require('./models/user')
+const { Todo } = require('./models/todo')
+const { User } = require('./models/user')
+
+var app = express()
+const port = process.env.PORT || 4000
+
+app.use(bodyParser.json())
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  })
+
+  todo.save().then(
+    doc => {
+      res.send(doc)
+    },
+    err => {
+      res.status(400).send(err)
+    }
+  )
+})
+
+app.get('/todos', (req, res) => {
+  Todo.find().then(
+    todos => {
+      res.send({ todos })
+    },
+
+    err => {
+      res.status(400).send(err)
+    }
+  )
+})
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send()
+      }
+      res.send({ todo })
+    })
+    .catch(e => {
+      res.status(400).send()
+    })
+})
+
+app.listen(port, () => {
+  console.log(`started on port ${port}`)
+})
+
+module.exports = { app }
---
+++ server/tests/server.test.js
@@ -0,0 +1,106 @@
+const expect = require('expect')
+const { ObjectID } = require('mongodb')
+
+const { app } = require('../server')
+const { Todo } = require('../models/todo')
+
+const todos = [
+  { _id: new ObjectID(), text: 'First test todo' },
+  { _id: new ObjectID(), text: 'Second test todo' }
+]
+
+beforeEach(done => {
+  Todo.deleteMany({})
+    .then(() => {
+      Todo.insertMany(todos)
+    })
+    .then(() => done())
+})
+
+describe('POST /todos', () => {
+  skip.it('should create a new todo', done => {
+    var text = 'test todo text'
+
+    request(app)
+      .post('/todos')
+      .send({ text })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.test).toBe(text)
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+
+        Todo.find(text)
+          .then(todos => {
+            expect(todos.length).toBe(1)
+            expect(todos[0].text).toBe(text)
+            done()
+          })
+          .catch(e => {
+            done(e) //note: using the statement syntax as apposed to the arrow func expression syntax
+          })
+      })
+  })
+
+  skip.it('should not create todo with invalid body data', done => {
+    request(app)
+      .post('/todos')
+      .send({})
+      .expect(400)
+      .end(err => {
+        if (err) {
+          return done(err)
+        }
+        Todo.find()
+          .then(todos => {
+            expect(todos.length).toBe(2)
+            done()
+          })
+          .catch(e => {
+            done(e)
+          })
+      })
+  })
+})
+
+describe('GET /todos', function() {
+  it('should get all todos', done => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.length).toBe(2)
+      })
+      .end(done)
+  })
+})
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', done => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.text).toBe(todos[0].text)
+      })
+      .end(done)
+  })
+  it('should return a 404 if todo is not found', done => {
+    var hexid = new ObjectID().toHexString()
+
+    request(app)
+      .get(`/todos/${hexid}`)
+      .expect(404)
+      .end(done)
+  })
+
+  it('should return a 404 for non-object ids', done => {
+    request(app)
+      .get(`/todos/123ttt`)
+      .expect(404)
+      .end(done)
+  })
+})
