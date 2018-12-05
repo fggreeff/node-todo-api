d60f60df761b36267cb27bf418b76326e1d28894
Francois Greeff <francois.greeff@bglgroup.co.uk>
2018-12-05 16:06:45 +0000

    Add PATCH todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const express = require('express')
 const bodyParser = require('body-parser')
 const { ObjectID } = require('mongodb')
@@ -63,6 +64,32 @@ app.delete('/todos/:id', (req, res) => {
   }
 
   Todo.findByIdAndRemove(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send()
+      }
+      res.send({ todo })
+    })
+    .catch(e => {
+      res.status(400).send()
+    })
+})
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id
+  //Use lodash to cherry pick subset properties from the body we want to allow updates on
+  var body = _.pick(req.body, ['text', 'completed'])
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime()
+  } else {
+    body.completed = false
+    body.completedAt = null //this removes value from DB
+  }
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
     .then(todo => {
       if (!todo) {
         return res.status(404).send()
