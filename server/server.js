9cd13ab9a64374c57a126cbeead07ab01c82d7f7
Francois Greeff <francois.greeff@bglgroup.co.uk>
2018-12-05 19:47:47 +0000

    Add custom token header. Refactor user schema

--- server/server.js
+++ server/server.js
@@ -110,8 +110,11 @@ app.post('/users', (req, res) => {
 
   user
     .save()
-    .then(user => {
-      res.send(user)
+    .then(() => {
+      return user.generateAuthToken()
+    })
+    .then(token => {
+      res.header('x-auth', token).send(user) // x- customer header
     })
     .catch(e => {
       res.status(400).send(e)
