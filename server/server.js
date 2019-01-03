a395813aa0c30d6c761ff27d149970bd68491a0d
Francois Greeff <francois.greeff@bglgroup.co.uk>
2019-01-03 12:42:40 +0000

    Refactor GET /users/me and add authentication middleware

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/todo')
 const { User } = require('./models/user')
 
+const { authenticate } = require('./middleware/authenticate')
+
 var app = express()
 const port = process.env.PORT
 
@@ -121,19 +123,8 @@ app.post('/users', (req, res) => {
     })
 })
 
-app.get('/users/me', (req, res) => {
-  var token = req.header('x-auth')
-
-  User.findByToken(token)
-    .then(user => {
-      if (!user) {
-        return Promise.reject()
-      }
-      res.send(user)
-    })
-    .catch(e => {
-      res.status(401).send()
-    })
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user)
 })
 
 app.listen(port, () => {
